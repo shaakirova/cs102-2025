2b0ba22944de1b99a3ca99ecce926639e4054591
aliya <aliya.shakirovaaa@inbox.ru>
2025-12-24 06:53:36 +0300

    Гарантированный путь в графическом лабиринте и изменение цвета пути

--- homework03/maze_gui.py
+++ homework03/maze_gui.py
@@ -3,6 +3,15 @@ from typing import List
 from tkinter import ttk, messagebox
 from maze import bin_tree_maze, solve_maze, add_path_to_grid
 
+CELL_SIZE = 10
+N, M = 51, 77
+
+def generate_maze_with_path(rows=N, cols=M):
+    while True:
+        grid = bin_tree_maze(rows, cols)
+        _, path = solve_maze(grid)
+        if path:
+            return grid, path
 
 def draw_cell(x, y, color, size: int = 10):
     x *= size
@@ -20,34 +29,26 @@ def draw_maze(grid: List[List[str]], size: int = 10):
             elif cell == "■":
                 color = 'black'
             elif cell == "X":
-                color = "red"
+                color = "purple"
             draw_cell(y, x, color, size)
 
 
 def show_solution():
-    maze, path = solve_maze(GRID)
-    maze = add_path_to_grid(GRID, path)
-    if path:
-        draw_maze(maze, CELL_SIZE)
-    else:
-        tk.messagebox.showinfo("Message", "No solutions")
-
+    maze_with_path = add_path_to_grid(GRID, PATH)
+    draw_maze(maze_with_path)
 
 if __name__ == "__main__":
-    global GRID, CELL_SIZE
-    N, M = 51, 77
+    GRID, PATH = generate_maze_with_path(N, M)
 
-    CELL_SIZE = 10
-    GRID = bin_tree_maze(N, M)
 
     window = tk.Tk()
     window.title('Maze')
-    window.geometry("%dx%d" % (M * CELL_SIZE + 100, N * CELL_SIZE + 100))
+    window.geometry(f"{M * CELL_SIZE + 100}x{N * CELL_SIZE + 100}")
 
     canvas = tk.Canvas(window, width=M * CELL_SIZE, height=N * CELL_SIZE)
     canvas.pack()
 
-    draw_maze(GRID, CELL_SIZE)
+    draw_maze(GRID)
     ttk.Button(window, text="Solve", command=show_solution).pack(pady=20)
 
     window.mainloop()
