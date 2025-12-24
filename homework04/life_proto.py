d754fa755f73c240173bb8b2359d8d313b57f73d
aliya <aliya.shakirovaaa@inbox.ru>
2025-12-24 12:31:34 +0300

    black

--- homework04/life_proto.py
+++ homework04/life_proto.py
@@ -10,8 +10,7 @@ Grid = tp.List[Cells]
 
 
 class GameOfLife:
-    def __init__(self, width: int = 640, height: int = 480, cell_size: int = 10, speed: int = 10
-    ) -> None:
+    def __init__(self, width: int = 640, height: int = 480, cell_size: int = 10, speed: int = 10) -> None:
         self.width = width
         self.height = height
         self.cell_size = cell_size
